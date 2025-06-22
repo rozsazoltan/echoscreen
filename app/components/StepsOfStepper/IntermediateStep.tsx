f198c4ba250f51528885c9bc7c594bb15ccb5dd6
Rózsa Zoltán <67325669+rozsazoltan@users.noreply.github.com>
2025-06-22 15:39:31 +0200

    remove connec-test-device btn

--- app/components/StepsOfStepper/IntermediateStep.tsx
+++ app/components/StepsOfStepper/IntermediateStep.tsx
@@ -92,25 +92,6 @@ export default function IntermediateStep(props: IntermediateStepProps) {
         handleNextApplicationWindow,
         connectedDevice
       )}
-      {
-        // eslint-disable-next-line no-nested-ternary
-        process.env.NODE_ENV === 'production' &&
-        process.env.RUN_MODE !== 'dev' &&
-        process.env.RUN_MODE !== 'test' ? (
-          <></>
-        ) : activeStep === 0 ? (
-          // eslint-disable-next-line react/jsx-indent
-          <Button
-            onClick={() => {
-              // connectedDevicesService.setPendingConnectionDevice(DEVICES[Math.floor(Math.random() * DEVICES.length)]);
-            }}
-          >
-            Connect Test Device
-          </Button>
-        ) : (
-          <></>
-        )
-      }
       {activeStep !== 0 ? (
         <Row>
           <Col xs={12}>
