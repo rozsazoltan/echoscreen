5db2c6e1ffffec4895ecdabbb751cc9cb6004740
Rózsa Zoltán <67325669+rozsazoltan@users.noreply.github.com>
2025-06-23 23:51:24 +0200

    Update HomePage.tsx

--- app/containers/HomePage.tsx
+++ app/containers/HomePage.tsx
@@ -4,7 +4,8 @@
 
 import { shell } from 'electron';
 import React, { useRef, useState } from 'react';
-import { Classes, Callout, Text, Button } from '@blueprintjs/core';
+import { Classes, Callout, Text, Button, Dialog } from '@blueprintjs/core';
+import { Row, Grid } from 'react-flexbox-grid';
 import { ToastProvider, DefaultToast } from 'react-toast-notifications';
 import { useTranslation } from 'react-i18next';
 import useAppVersions from '../hooks/useAppVersions';
@@ -56,43 +57,49 @@ export default function HomePage() {
       <div className={Classes.TREE}>
         <TopPanel stepperRef={stepperRef} />
         <EchoScreenStepper ref={stepperRef} />
-
-        {shouldShowUpdate && (
-          <Callout
-            id="new-version-header"
-            intent="primary"
-            title={t('v{{version}}', { version: latestVersion })}
-          >
-            <Text>
-              {t(
-                'A new stable version of EchoScreen is available! Using the latest version ensures better performance, new features, and important security fixes.',
-              )}
-            </Text>
-            <Button
-              text={t('Update Now')}
-              icon="cloud-download"
-              intent="primary"
-              large
-              style={{ margin: '0.7rem 0' }}
-              onClick={(e: any) => {
-                e.preventDefault();
-                shell.openExternal('https://github.com/rozsazoltan/echoscreen/releases/latest');
-              }}
-            />
-            <Button
-              text={t('Later')}
-              icon="heart-broken"
-              intent="none"
-              small
-              style={{ margin: '0.7rem 0.5rem', padding: '0.4rem 0.6rem' }}
-              onClick={(e: any) => {
-                e.preventDefault();
-                setShowUpdateMessage(false);
-              }}
-            />
-          </Callout>
-        )}
       </div>
+
+      <Dialog isOpen={shouldShowUpdate} autoFocus usePortal>
+        <Grid>
+          <div>
+            <Row style={{ marginTop: '20px' }}>
+              <Callout
+                id="new-version-header"
+                intent="primary"
+                title={t('v{{version}}', { version: latestVersion })}
+              >
+                <Text>
+                  {t(
+                    'A new stable version of EchoScreen is available! Using the latest version ensures better performance, new features, and important security fixes.',
+                  )}
+                </Text>
+                <Button
+                  text={t('Update Now')}
+                  icon="cloud-download"
+                  intent="primary"
+                  large
+                  style={{ margin: '0.7rem 0' }}
+                  onClick={(e: any) => {
+                    e.preventDefault();
+                    shell.openExternal('https://github.com/rozsazoltan/echoscreen/releases/latest');
+                  }}
+                />
+                <Button
+                  text={t('Later')}
+                  icon="heart-broken"
+                  intent="none"
+                  small
+                  style={{ margin: '0.7rem 0.5rem', padding: '0.4rem 0.6rem' }}
+                  onClick={(e: any) => {
+                    e.preventDefault();
+                    setShowUpdateMessage(false);
+                  }}
+                />
+              </Callout>
+            </Row>
+          </div>
+        </Grid>
+      </Dialog>
     </ToastProvider>
   );
 }
